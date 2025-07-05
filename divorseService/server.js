6c6c205a7dc69d22d842e08ee73f3efbc07a84ef
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2025-07-05 23:11:24 +0200

    eleventh

--- divorseService/server.js
+++ divorseService/server.js
@@ -1,23 +1,19 @@
 // server.js
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json()); // для JSON
-app.use(bodyParser.urlencoded({ extended: true })); // для form-urlencoded
+app.use(cors()); // Разрешает запросы с других источников
+app.use(express.json()); // Обработка JSON
+app.use(express.static('public')); // Отдаем HTML, CSS, картинки и т.д.
 
-// Приём POST-запроса с формы
 app.post('/submit-alimony', (req, res) => {
-  console.log('Получены данные формы:', req.body);
-  res.json({ success: true, message: 'Данные получены успешно!' });
+  console.log('Данные формы:', req.body);
+  res.json({ status: 'ok' });
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
